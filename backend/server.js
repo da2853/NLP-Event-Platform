4d250d04e581a699d8cf35e590714cd1fe85daf6
Danyal Ahmad <100463162+da2853@users.noreply.github.com>
2024-05-22 01:28:28 -0400

    testing

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fastify_1 = __importDefault(require("fastify"));
+const server = (0, fastify_1.default)({ logger: true });
+server.get('/', (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
+    reply.type('application/json').code(200);
+    return { hello: 'world' };
+}));
+const start = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield server.listen({ port: 3000 });
+        const address = server.server.address();
+        if (address && typeof address !== 'string') {
+            console.log(`Server listening at http://localhost:${address.port}`);
+        }
+        else {
+            console.log(`Server listening at ${address}`);
+        }
+    }
+    catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+});
+start();
---
+++ backend/server.ts
@@ -0,0 +1,25 @@
+import fastify from 'fastify';
+
+const server = fastify({ logger: true });
+
+server.get('/', async (request, reply) => {
+  reply.type('application/json').code(200);
+  return { hello: 'world' };
+});
+
+const start = async () => {
+  try {
+    await server.listen({ port: 3000 });
+    const address = server.server.address();
+    if (address && typeof address !== 'string') {
+      console.log(`Server listening at http://localhost:${address.port}`);
+    } else {
+      console.log(`Server listening at ${address}`);
+    }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+start();
